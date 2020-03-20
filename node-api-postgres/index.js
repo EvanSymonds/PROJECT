0a304e1459ddf63b716ff74223cbd8da1807240c
EvanSymonds <evan.symonds@gmail.com>
2020-03-20 23:17:16 +0000

    commit

--- node-api-postgres/index.js
+++ node-api-postgres/index.js
@@ -1,13 +1,10 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const fileUpload = require("express-fileupload")
-const morgan = require('morgan');
 const db = require("./queries")
 const app = express()
 const port = 3000
 
 app.use(bodyParser.json({limit: "50mb"}))
-app.use(morgan('dev'));
 app.use(
   bodyParser.urlencoded({
     limit: "50mb",
@@ -15,9 +12,6 @@ app.use(
     parameterLimit: 1000000,
   })
 )
-app.use(fileUpload({
-  createParentPath: true
-}));
 
 app.get("/", (request, response) => {
   response.json({ info: 'Node.js, Express and Postgres API' })
