5ddeab0432c6bddb1573362174719fc99e530fa5
EvanSymonds <evan.symonds@gmail.com>
2020-05-31 19:38:47 +0100

    Deploying

--- server/index.js
+++ server/index.js
@@ -39,7 +39,6 @@ app.use(bodyParser.urlencoded({
     parameterLimit: 1000000,
   })
 )
-app.use(helmet());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
