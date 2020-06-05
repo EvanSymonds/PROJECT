fc5a367bd727b8d8f04dd17a2e66bcd9f6a8a237
EvanSymonds <evan.symonds@gmail.com>
2020-06-05 16:24:32 +0100

    Removed build from gitignore

---
+++ client/build/precache-manifest.928dcf33329ac7293c5da87daf564748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e99d70217554c57ddcb1825d8a5e49b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4126add88306228f70a5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9fd619f8235bfcef6dbb",
+    "url": "/static/js/2.0cb936e6.chunk.js"
+  },
+  {
+    "revision": "884d6422099fcad6c31f436a36163b39",
+    "url": "/static/js/2.0cb936e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4126add88306228f70a5",
+    "url": "/static/js/main.341c1a8e.chunk.js"
+  },
+  {
+    "revision": "3186ac95b833a6435728",
+    "url": "/static/js/runtime-main.aee3e6f9.js"
+  }
+]);
