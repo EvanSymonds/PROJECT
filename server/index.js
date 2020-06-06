ee18a2bf8853c89b34544c632d7ddc3ad79ca61d
EvanSymonds <evan.symonds@gmail.com>
2020-06-06 15:38:24 +0100

    Cleaning up a few things

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ if(!config.get("jwtPrivateKey")) {
 }
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")))
+  app.use(express.static(path.join(__dirname, "..", "client", "build")))
   
   app.get("/favicon.ico", (req, res) => {
     res.sendFile("favicon.ico");
