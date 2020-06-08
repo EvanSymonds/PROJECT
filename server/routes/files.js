c83d67788f169615f274ada0064da1d49b265de8
EvanSymonds <evan.symonds@gmail.com>
2020-06-08 18:37:59 +0100

    Troubleshooting file download bug

--- server/routes/files.js
+++ server/routes/files.js
@@ -26,6 +26,8 @@ router.get("/", async (request, response) => {
 
 router.get("/:id", async (request, response) => {
 
+  console.log("Endpoint reached")
+
   await file_api.getFileById(parseInt(request.params.id)).then(async (file) => {
     response.setHeader("Content-Disposition", "attachment; filename=" + file[0].file_name)
     response.set('Content-Type', 'text/csv');
